25aa1b17ddebe861250956efb9a8cec94ce2db66
Franco Manuel Delucchi Diaz <fdelucch@car1s2.42barcelona.com>
2026-06-19 20:00:39 +0200

    ft_putnbr con return quizas no permitido

--- c_04/ex02/ft_putnbr.c
+++ c_04/ex02/ft_putnbr.c
@@ -1,31 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fdelucch <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/19 19:56:07 by fdelucch          #+#    #+#             */
+/*   Updated: 2026/06/19 19:59:49 by fdelucch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
 
 void	ft_putnbr(int nb)
 {
 	char	c;
-	int		do_nothing;
 
-	do_nothing = 0;
 	if (nb == -2147483648)
 	{
 		write(1, "-2147483648", 11);
-		do_nothing = 1;
+		return ;
 	}
-	if (nb < 0 && do_nothing != 1)
+	if (nb < 0)
 	{
 		write(1, "-", 1);
 		nb *= -1;
 	}
-	if (nb < 10 && do_nothing != 1)
+	if (nb < 10)
 		c = nb + '0';
-		write()
+	else
+	{
+		ft_putnbr((nb / 10));
+		c = (nb % 10) + '0';
+	}
+	write(1, &c, 1);
 }
-int	main(void)
+/*int	main(void)
 {
 	ft_putnbr(42);
+	ft_putnbr(1);
 	ft_putnbr(0);
 	ft_putnbr(-42);
 	ft_putnbr(-2147483648);
 	ft_putnbr(2147483647);
 	return (0);
 }
+*/
