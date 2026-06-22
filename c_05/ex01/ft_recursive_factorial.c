ddb7e2a26d8954ab51cdadcf0cd8dc6931ff95fd
Franco Manuel Delucchi Diaz <fdelucch@cbr12s3.42barcelona.com>
2026-06-22 17:57:02 +0200

    factorial recursivo

--- c_05/ex01/ft_recursive_factorial.c
+++ c_05/ex01/ft_recursive_factorial.c
@@ -3,15 +3,12 @@
 
 int	ft_recursive_factorial(int nb)
 {
-	int	res;
-
 	if (nb < 0)
 		return (0);
 	if (nb > 1)
-	{
-		res = (nb * ft_recursive_factorial(nb - 1));
-	}
-	return (res);
+		return (nb * ft_recursive_factorial(nb - 1));
+	else
+		return (1);
 }
 int	main(void)
 {
